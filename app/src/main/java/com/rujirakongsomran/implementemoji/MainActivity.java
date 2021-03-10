41d0e0f4995c6e6cecc812f53547935c61deb9ff
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-03-10 16:30:36 +0700

    Enable ViewBinding

--- app/src/main/java/com/rujirakongsomran/implementemoji/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/implementemoji/MainActivity.java
@@ -3,12 +3,19 @@ package com.rujirakongsomran.implementemoji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rujirakongsomran.implementemoji.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //setContentView(R.layout.activity_main);
     }
 }
