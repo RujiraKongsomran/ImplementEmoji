045b161572383d698b133e7975f06130cca00c81
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-03-10 17:30:37 +0700

    Finish

---
+++ app/src/main/java/com/rujirakongsomran/implementemoji/EmojiApplication.java
@@ -0,0 +1,15 @@
+package com.rujirakongsomran.implementemoji;
+
+import android.app.Application;
+
+import com.vanniktech.emoji.EmojiManager;
+import com.vanniktech.emoji.google.GoogleEmojiProvider;
+
+public class EmojiApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Install emoji manager
+        EmojiManager.install(new GoogleEmojiProvider());
+    }
+}
--- app/src/main/java/com/rujirakongsomran/implementemoji/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/implementemoji/MainActivity.java
@@ -3,9 +3,12 @@ package com.rujirakongsomran.implementemoji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.rujirakongsomran.implementemoji.databinding.ActivityMainBinding;
+import com.vanniktech.emoji.EmojiPopup;
+import com.vanniktech.emoji.EmojiTextView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -17,5 +20,35 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         //setContentView(R.layout.activity_main);
+
+        // Initialize emoji popup
+        EmojiPopup popup = EmojiPopup.Builder.fromRootView(
+                findViewById(R.id.root_view)
+        ).build(binding.etEmoji);
+
+        binding.ivEmoji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle between text and emoji
+                popup.toggle();
+            }
+        });
+
+        binding.ivSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inflate emoji text view
+                EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
+                        .from(view.getContext())
+                        .inflate(R.layout.emoji_text_view, binding.linearLayout, false);
+
+                // Set Text on emoji
+                emojiTextView.setText(binding.etEmoji.getText().toString());
+                // Add view to linear layout
+                binding.linearLayout.addView(emojiTextView);
+                // Clear edit text value
+                binding.etEmoji.getText().clear();
+            }
+        });
     }
 }
